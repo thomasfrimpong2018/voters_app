428b46bd5750148752126e07a204da4e41d0fd7f
Thomas Frimpong <tfrimpong1987@gmail.com>
2018-06-19 09:41:28 +0100

    a function is created to redirect the user/admin to the right dashboard

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+use Auth;
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+
+    //
+    public function index(){
+
+        if(Auth::user()->admin == 0){
+            return view('home');
+   
+        }else{
+            $users['users']=\App\User::all();
+           return view('admin.adminhome',$users); 
+        }
+
+    }
+}
