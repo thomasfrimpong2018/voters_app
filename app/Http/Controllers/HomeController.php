5b06b02230bb1416944f781fb1382f7d2bdf088e
Thomas Frimpong <tfrimpong1987@gmail.com>
2018-06-19 09:40:22 +0100

    a function to add a presidential aspirant is added

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\President;
+use Auth;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -21,8 +22,51 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+
+    public function addSecretary()
     {
         return view('home');
     }
+
+   
+
+
+    public function addPresident(Request $request){
+
+
+        $this->validate($request, [
+            'name'=>'required',
+            'cover_image'=>'image|nullable|max:1999'
+        ]);
+
+
+
+         //Handle File Upload
+         if($request->hasFile('cover_image')){
+
+            //Get Filename with extension
+            $fileNameWithExt=$request->file('cover_image')->getClientOriginalName();
+            //Get just file
+            $filename=pathinfo($fileNameWithExt,PATHINFO_FILENAME);
+            //Get the ext
+            $extension=$request->file('cover_image')->getClientOriginalExtension();
+            //Filename to store
+            $fileNameToStore=$filename.'_'.time().'.'.$extension;
+            //Upload Image
+            $path=$request->file('cover_image')->storeAs('public/cover_image',$fileNameToStore);
+
+        }else{
+              $fileNameToStore='noimage.jpg';
+        }
+         
+        $president=new President;
+        $president->name=$request->name;
+        $president->picture= $fileNameToStore;
+        $president->save();
+
+        return redirect('/home');
+    }
+
+
+    
 }
