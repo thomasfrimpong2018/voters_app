7fc6e74e7c2bbac0c4a123cef3f4bab77591d2f4
Thomas Frimpong <tfrimpong1987@gmail.com>
2018-06-19 09:42:42 +0100

    a navbar is created for the admin dashboard

---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,40 @@
+<nav class="col-md-2 d-none d-md-block bg-light sidebar">
+        <div class="sidebar-sticky">
+          <ul class="nav flex-column">
+            <li class="nav-item">
+              <a class="nav-link active" href="#">
+                <span data-feather="home"></span>
+                Add A Presidential Aspirant<span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <span data-feather="file"></span>
+                Add A Secretarial Aspirant
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <span data-feather="shopping-cart"></span>
+                Add A Organiser Aspirant
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <span data-feather="users"></span>
+                Add A Treasurer Aspirant
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <span data-feather="bar-chart-2"></span>
+                Add Another  Aspirant
+              </a>
+            </li>
+            
+          </ul>
+
+          
+         
+        </div>
+      </nav>
