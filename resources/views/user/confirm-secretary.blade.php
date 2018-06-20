03d6b509f132554294d593c16c6c133de1b2d617
Thomas Frimpong <tfrimpong1987@gmail.com>
2018-06-20 09:21:01 +0100

    page to confirm a secretary being voted for

---
+++ resources/views/user/confirm-secretary.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-7">
+            <div class="card">
+               <div class="card-header">Confirm Vote </div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+     <div class="container">
+        Are you Sure You Want to Vote For '{{$secretary->name}}' ? <br>
+
+      {!!Form::open(['action'=>['HomeController@votesecretary',$secretary->id],'method'=>'POST'])!!}
+      {{Form::hidden('id',$secretary->id)}}
+      {{Form::hidden('votes',$secretary->votes)}}
+      {{Form::submit('Yes',['class'=>'btn btn-success'])}}
+
+      {!!Form::close()!!}
+
+      <div  style="float:left"> 
+      <a href="/home" class="btn btn-danger  ">No</a>
+      </div>
+
+      <footer class="pt-4 my-md-5 pt-md-5 border-top">
+        <div class="row">
+          </div>
+      </footer>
+    </div>
+ 
+                
+
+
+
+                   
+
+
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
